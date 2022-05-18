2c915d578b23d5836e27aa3c0a6342bc4d4fae52
Davide <franc.davi00@gmail.com>
2022-05-18 22:51:54 +0200

    aggiunta commenti

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.*/
 
-
+//dichiaro l' array di oggetti
 const azienda = 
 [
     {
@@ -42,37 +42,47 @@ const azienda =
     }
 
 ];
- let container = document.querySelector('.team-container');
+
+//faccio una selezione sulla classe
+let container = document.querySelector('.team-container');
  
+
+//creo il for per stampare i dipendenti
  for (let i = 0; i < azienda.length; i ++) {
     let stuff = azienda[i];
 
+    //creo l' elemento card che farà da container e lo collego a container
     const card = document.createElement('div');
     card.classList.add('team-card');
     container.append(card);
     
+    //creo l' elemnto img collegandolo a card, e gli aggiungo il tag
     const img = document.createElement('div');
     img.classList.add('card-image');
 
+    //creo l'elemento profilo e lo appendo nel tag
     const profilo = document.createElement("img");
     profilo.src = stuff.foto;
-    img.appendChild(profilo);
+    img.append(profilo);
+
+    //appendo img a card
+    card.append(img);
 
     const text = document.createElement('div');
     text.classList.add('card-text');
 
+    //creo gli elemnti h3 e p a cui verranno appesi nome e ruolo
     const Nome = document.createElement("h3");
     const Ruolo = document.createElement("p");
-    text.appendChild(Nome);
-    text.appendChild(Ruolo);
+    text.append(Nome);
+    text.append(Ruolo);
 
-    card.append(img);
+    //appendo text a card
     card.append(text);
 
+    //richiamo gli oggetti 
     Nome.innerText = stuff.nome; 
     Ruolo.innerText = stuff.ruolo;
-
-    /* img.src = stuff.foto; */
 
     console.log(card);
  }
